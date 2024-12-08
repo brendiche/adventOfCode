cef1db2a8a084f48afbbab224583ba8992bdec0e
brendiche <brendan.gouin92@gmail.com>
2024-12-08 12:38:41 +0100

    remove debug files

--- 2024/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/brendiche/workspace/adventOfCode/2024/day1/target/debug/day1: /home/brendiche/workspace/adventOfCode/2024/day1/src/main.rs
--- 2024/day1/target/debug/deps/day1-fbae7e84d8188cbd.d
+++
@@ -1,5 +0,0 @@
-/home/brendiche/workspace/adventOfCode/2024/day1/target/debug/deps/libday1-fbae7e84d8188cbd.rmeta: src/main.rs
-
-/home/brendiche/workspace/adventOfCode/2024/day1/target/debug/deps/day1-fbae7e84d8188cbd.d: src/main.rs
-
-src/main.rs:
